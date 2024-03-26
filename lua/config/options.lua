09da8dbd8aca9bd9ebaabf5cb464a4290383fb1f
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2024-03-26 08:41:55 -0300

    config nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.fillchars = { eob = " " }
+
+vim.opt.cursorline = false
+vim.opt.cursorlineopt = 'number'
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 4
+vim.opt.wrap = false
